fcb0283f4863f5b084f3e4a7598c208fc8062581
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-06-18 23:09:38 +0530

    added name, utils, alternate files for modules section.

---
+++ 01-node-tutorial/05-alternate-flavour.js
@@ -0,0 +1,8 @@
+const item = ['item1', 'item2'];
+const person = {
+    name: 'Bob',
+    age : 36
+}
+// console.log(item , person);
+module.exports = item;
+module.exports= person;
---
+++ 01-node-tutorial/app.js
@@ -0,0 +1,7 @@
+const names = require('./03-name');
+const sayHi = require('./04-utils');
+const data = require('./05-alternate-flavour')
+
+sayHi("Barkha");
+sayHi(names.john);
+sayHi(names.peter);
