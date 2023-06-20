7bee9c8500ad8c95eb426cb50d574615434b2d2c
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-06-20 21:02:33 +0530

    Update app.js
    added require mind-grenade.js

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,6 +1,8 @@
 const names = require('./03-name');
 const sayHi = require('./04-utils');
 const data = require('./05-alternate-flavour')
+require('./06-mind-grenade')
+
 
 sayHi("Barkha");
 sayHi(names.john);
