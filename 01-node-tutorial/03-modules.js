4fb2871547908c50d6067e51f2710c4254116ab1
Abhijit Sahoo <abhijitsahooas.01@gmail.com>
2023-06-20 21:40:03 +0530

    Added module, os, path modules code files.
    created module.js to store previous imports, and written os modules and path modules code.

---
+++ 01-node-tutorial/03-modules.js
@@ -0,0 +1,9 @@
+const names = require('./04-name');
+const sayHi = require('./05-utils');
+const data = require('./06-alternate-flavour')
+require('./07-mind-grenade')
+
+
+sayHi("Sushan");
+sayHi(names.john);
+sayHi(names.peter);
